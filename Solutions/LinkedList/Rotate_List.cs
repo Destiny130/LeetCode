dda3ea2b24c0381eb72047ad8a09069fd56f4b43
destiny130 <destiny1307654641@gamil.com>
2019-03-09 20:05:18 +0800

    Upgrade

--- Solutions/LinkedList/Rotate_List.cs
+++ Solutions/LinkedList/Rotate_List.cs
@@ -2,31 +2,33 @@ namespace Solutions.LinkedList
 {
     public class Rotate_List
     {
-        public ListNode RotateRight(ListNode head, int k) {
-        if(head == null || k == 0)
-            return head;
-        int len = 0;
-        ListNode curr = head;
-        for(; curr != null; ++len)
-            curr = curr.next;
-        k %= len;
-        if(k == 0)
-            return head;
-        ListNode fast = head;
-        --k;
-        for(; k > 0; --k)
-            fast = fast.next;
-        
-        curr = head;
-        ListNode prev = null;
-        while(fast.next != null){
-            fast = fast.next;
-            prev = curr;
-            curr = curr.next;
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null || k == 0)
+                return head;
+            int len = 0;
+            ListNode curr = head;
+            for (; curr != null; ++len)
+                curr = curr.next;
+            k %= len;
+            if (k == 0)
+                return head;
+            ListNode fast = head;
+            --k;
+            for (; k > 0; --k)
+                fast = fast.next;
+
+            curr = head;
+            ListNode prev = null;
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                prev = curr;
+                curr = curr.next;
+            }
+            fast.next = head;
+            prev.next = null;
+            return curr;
         }
-        fast.next = head;
-        prev.next = null;
-        return curr;
-    }
     }
 }
