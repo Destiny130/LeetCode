dd0d280838344984a03fbafea320b253fab91b96
Destiny130 <destiny1307654641@hotmail.com>
2019-03-09 18:15:30 +0800

    Create AddTwoNumbers.cs

---
+++ LinkedList/AddTwoNumbers.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode temp = dummyNode;
+        int factor = 0;
+        while(l1 != null || l2 != null){
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int sum = v1 + v2 + factor;
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
+            factor = sum / 10;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        if(factor == 1){
+            temp.next = new ListNode(1);
+        }
+        return dummyNode.next;
+    }
+}
