cfd298e139d8a9b782bb9772d8633647614bd2df
Destiny130 <destiny1307654641@hotmail.com>
2019-03-09 18:11:43 +0800

    Create \Array\TwoSum.cs

---
+++ \Array\TwoSum.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; ++i){
+            if(dic.ContainsKey(target - nums[i]) && dic[target - nums[i]] != i)
+                return new int[] { dic[target - nums[i]], i };
+            dic[nums[i]] = i;
+        }
+        return new int[] {};
+    }
+}
