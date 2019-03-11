6483225430a8237512bad853ac1c28cfb946020e
Destiny130 <destiny1307654641@hotmail.com>
2019-03-11 08:45:01 +0800

    Create N_Repeated_Element_in_Size_2N_Array.cs

---
+++ Solutions/HashTable/N_Repeated_Element_in_Size_2N_Array.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.HashTable
+{
+    public class N_Repeated_Element_in_Size_2N_Array
+    {
+        public int RepeatedNTimes(int[] A)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (int i in A)
+            {
+                if (dic.ContainsKey(i))
+                    dic[i] += 1;
+                else
+                    dic.Add(i, 1);
+            }
+            return dic.Where(d => d.Value == (A.Length / 2)).First().Key;
+        }
+
+        //Compare
+        public int RepeatedNTimes_Compare(int[] A)
+        {
+            for (int i = 2; i < A.Length; ++i)
+                if (A[i] == A[i - 1] || A[i] == A[i - 2])
+                    return A[i];
+            return A[0];
+        }
+    }
+}
