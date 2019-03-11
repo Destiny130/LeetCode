82e42ddd5af1448399988391c73e34c149a455a3
Destiny130 <destiny1307654641@hotmail.com>
2019-03-11 20:46:34 +0800

    Update

---
+++ Solutions/HashTable_LC/Four_Sum_II.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.HashTable_LC
+{
+    public class Four_Sum_II
+    {
+        public int FourSumCount(int[] A, int[] B, int[] C, int[] D)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (int a in A)
+            {
+                foreach (int b in B)
+                {
+                    if (dic.ContainsKey(a + b))
+                        ++dic[a + b];
+                    else
+                        dic[a + b] = 1;
+                }
+            }
+
+            int count = 0;
+            foreach (int c in C)
+            {
+                foreach (int d in D)
+                {
+                    if (dic.ContainsKey(-c - d))
+                    {
+                        count += dic[-c - d];
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
