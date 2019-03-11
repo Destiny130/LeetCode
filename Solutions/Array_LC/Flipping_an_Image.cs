0c5b301af7fe73bb6f18b5262323ad097364eb22
Destiny130 <destiny1307654641@hotmail.com>
2019-03-11 21:47:11 +0800

    Update

---
+++ Solutions/Array_LC/Flipping_an_Image.cs
@@ -0,0 +1,29 @@
+﻿namespace Solutions.Array_LC
+{
+    public class Flipping_an_Image
+    {
+        public int[][] FlipAndInvertImage(int[][] A)
+        {
+            int[][] result = new int[A.Length][];
+            for (int i = 0; i < A.Length; ++i)
+            {
+                int[] arr = new int[A[i].Length];
+                for (int j = arr.Length - 1; j != -1; --j)
+                    arr[arr.Length - 1 - j] = A[i][j] ^ 1;
+                result[i] = arr;
+            }
+            return result;
+        }
+
+        private void Reverse(int[] arr)
+        {
+            int low = 0, high = arr.Length - 1;
+            while (low < high)
+            {
+                arr[low] ^= arr[high];
+                arr[high] ^= arr[low];  //low;
+                arr[low] ^= arr[high];  //high
+            }
+        }
+    }
+}
---
+++ Solutions/BinaryTree_LC/Increasing_Order_Search_Tree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.BinaryTree_LC
+{
+    public class Increasing_Order_Search_Tree
+    {
+        public TreeNode IncreasingBST(TreeNode root)
+        {
+            if (root == null)
+                return null;
+            List<int> list = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            while (root != null || stack.Count != 0)
+            {
+                while (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+                root = stack.Pop();
+                list.Add(root.val);
+                root = root.right;
+            }
+            root = new TreeNode(list[0]);
+            TreeNode temp = root;
+            for (int i = 1; i < list.Count; ++i)
+            {
+                temp.right = new TreeNode(list[i]);
+                temp = temp.right;
+            }
+            return root;
+        }
+    }
+}
