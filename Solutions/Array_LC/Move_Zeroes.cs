c1121c5a49d0dc5b2ce92716399e8ba0b963b84e
Destiny130 <destiny1307654641@hotmail.com>
2019-03-13 23:07:44 +0800

    Update

--- Solutions/Array_LC/Move_Zeroes.cs
+++ Solutions/Array_LC/Move_Zeroes.cs
@@ -13,5 +13,21 @@
             for (; k < nums.Length; ++k)
                 nums[k] = 0;
         }
+
+        //One loop
+        public void MoveZeroes_1(int[] nums)
+        {
+            for (int k = 0, i = 0; i < nums.Length; ++i)
+            {
+                if (nums[i] != 0 && nums[k] == 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[k];
+                    nums[k++] = temp;
+                }
+                else if (nums[k] != 0)
+                    k = i;
+            }
+        }
     }
 }
