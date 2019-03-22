8beff5d23c4f1c940e0b9f4d368b254a89415d4d
Destiny130 <destiny1307654641@hotmail.com>
2019-03-22 09:23:49 +0800

    Update Execute.cs

--- TestCases/Execute.cs
+++ TestCases/Execute.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using TestCases.Array_LC;
+using TestCases.DP_LC;
 
 namespace TestCases
 {
@@ -8,6 +9,8 @@ namespace TestCases
         static void Main()
         {
             new Two_Sum_Test().Execute();
+            new Word_Break_Test().Execute();
+
             Console.ReadKey();
         }
     }
