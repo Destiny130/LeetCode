2f980c5203eb8442659702cf91e64911a5c82c89
Destiny130 <destiny1307654641@hotmail.com>
2019-03-26 14:42:03 +0800

    Update House_Robber.cs

--- Solutions/DP_LC/House_Robber.cs
+++ Solutions/DP_LC/House_Robber.cs
@@ -8,6 +8,7 @@ namespace Solutions.DP_LC
         You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
         Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
         */
+
         public int Rob(int[] nums)
         {
             if (nums == null || nums.Length == 0)
@@ -22,6 +23,21 @@ namespace Solutions.DP_LC
                 max = Math.Max(max, nums[i]);
             }
             return max;
+        }
+
+        //Don't alter orig array
+        public int Rob_Optimize(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            int rob = 0, notRob = 0;
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int currRob = notRob + nums[i];
+                notRob = Math.Max(rob, notRob);
+                rob = currRob;
+            }
+            return Math.Max(rob, notRob);
         }
     }
 }
