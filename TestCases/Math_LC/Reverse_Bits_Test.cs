6614d1e26aa1e8a555a3fcfe0a3deca707a5ea6a
Destiny130 <destiny1307654641@hotmail.com>
2019-03-29 09:59:13 +0800

    Update Reverse_Bits_Test.cs

--- TestCases/Math_LC/Reverse_Bits_Test.cs
+++ TestCases/Math_LC/Reverse_Bits_Test.cs
@@ -7,8 +7,11 @@ namespace TestCases.Math_LC
     {
         public void Execute()
         {
+            Console.WriteLine($"\n{nameof(Reverse_Bits_Test)}");
             uint n = 43261596;
-            Console.WriteLine(new Reverse_Bits().reverseBits(n));
+            uint k = new Reverse_Bits().reverseBits(n);
+            Console.WriteLine($"Input nint: {n.ToString()}\nAfter reverse bits: {k}");
+            Console.WriteLine($"Input bits:\t{Convert.ToString(n, 2).PadLeft(32, '0')}\nReverse bits:\t{Convert.ToString(k, 2).PadLeft(32, '0')}");
         }
     }
 }
