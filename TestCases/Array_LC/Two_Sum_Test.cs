c559e463774cf179e07c45b5ebca10810953139a
Destiny130 <destiny1307654641@hotmail.com>
2019-03-29 09:58:18 +0800

    Update Two_Sum_Test.cs

--- TestCases/Array_LC/Two_Sum_Test.cs
+++ TestCases/Array_LC/Two_Sum_Test.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using Solutions.Array_LC;
 
 namespace TestCases.Array_LC
@@ -12,7 +11,8 @@ namespace TestCases.Array_LC
             int[] arr = { 2, 7, 11, 34, 3, 2 };
             int target = 9;
             int[] result = new Two_Sum().TwoSum(arr, target);
-            Console.WriteLine($"Input array: {String.Join(", ", arr)}, target: {target.ToString()}\nResult: {String.Join(", ", result)}");
+            Console.WriteLine($"Input array: {String.Join(", ", arr)}, target: {target.ToString()}");
+            Console.WriteLine($"Result: {String.Join(", ", result)}");
         }
     }
 }
