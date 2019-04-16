a32d5a782b4516b27e1e850bbb93066b7c493d63
Destiny130 <destiny1307654641@hotmail.com>
2019-04-16 14:15:18 +0800

    Create Longest_Increasing_Subsequence.cs

---
+++ Solutions/DP_LC/Longest_Increasing_Subsequence.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Solutions.DP_LC
+{
+    public class Longest_Increasing_Subsequence
+    {
+        /*
+        Given an unsorted array of integers, find the length of longest increasing subsequence.
+        */
+
+        public int LengthOfLIS(int[] nums)
+        {
+            if (nums == null)
+                return 0;
+            int len = nums.Length, max = 0;
+            if (len < 2)
+                return len;
+            int[] dp = new int[len];
+            dp[0] = 1;
+            for (int i = 1; i < len; ++i)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    if (nums[j] < nums[i])
+                        dp[i] = Math.Max(dp[i], dp[j]);
+                }
+                max = Math.Max(max, ++dp[i]);
+            }
+            return max;
+        }
+    }
+}
