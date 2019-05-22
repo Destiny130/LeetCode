de9f6a69240dea52348a75ac91fb5941578c5c85
Destiny130 <destiny1307654641@hotmail.com>
2019-05-22 10:55:16 +0800

    Update Merge_Intervals.cs

--- Solutions/Array_LC/Merge_Intervals.cs
+++ Solutions/Array_LC/Merge_Intervals.cs
@@ -33,6 +33,32 @@ namespace Solutions.Array_LC
             result.Add(new Interval(start, end));
             return result;
         }
+
+        public int[][] Merge(int[][] intervals)
+        {
+            List<int[]> result = new List<int[]>();
+            int len = 0;
+            if (intervals == null || (len = intervals.Length) == 0)
+                return result.ToArray();
+
+            Array.Sort(intervals, (x, y) => x[0] - y[0]);
+            int start = intervals[0][0], end = intervals[0][1];
+            for (int i = 1; i < len; ++i)
+            {
+                if (intervals[i][0] <= end)
+                {
+                    end = Math.Max(end, intervals[i][1]);
+                }
+                else
+                {
+                    result.Add(new int[] { start, end });
+                    start = intervals[i][0];
+                    end = intervals[i][1];
+                }
+            }
+            result.Add(new int[] { start, end });
+            return result.ToArray();
+        }
     }
 
     public class Interval
