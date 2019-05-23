40f8973d25b0fe4764b4a9d4603c74cdb9e1b8c7
Destiny130 <destiny1307654641@hotmail.com>
2019-05-23 16:37:39 +0800

    Update Search_a_2D_Matrix_II.cs

--- Solutions/Array_LC/Search_a_2D_Matrix_II.cs
+++ Solutions/Array_LC/Search_a_2D_Matrix_II.cs
@@ -10,21 +10,51 @@ namespace Solutions.Array_LC
 
         public bool SearchMatrix(int[,] matrix, int target)
         {
-            if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            if (matrix == null || matrix.Length == 0)
                 return false;
-            int m = matrix.GetLength(0);
-            int n = matrix.GetLength(1);
-            int i = 0, j = n - 1;
-            while (i < m && j >= 0)
+            int m = matrix.GetLength(0), n = matrix.GetLength(1);
+            int i = 0, j = m - 1;
+            while (i < n && j >= 0)
             {
-                if (matrix[i, j] == target)
+                if (matrix[j, i] == target)
                     return true;
-                else if (matrix[i, j] > target)
+                else if (matrix[j, i] > target)
                     --j;
                 else
                     ++i;
             }
             return false;
+        }
+
+        public bool SearchMatrix_DC(int[,] matrix, int target)
+        {
+            if (matrix == null || matrix.Length == 0)
+                return false;
+            return SearchMatrix(matrix, target, 0, matrix.GetLength(0) - 1, 0, matrix.GetLength(1) - 1);
+        }
+
+        private bool SearchMatrix(int[,] matrix, int target, int rowLow, int rowHigh, int columnLow, int columnHigh)
+        {
+            if (rowLow > rowHigh || columnLow > columnHigh)
+                return false;
+
+            int rowMid = rowLow + (rowHigh - rowLow) / 2, columnMid = columnLow + (columnHigh - columnLow) / 2;
+            int value = matrix[rowMid, columnMid];
+            if (value == target)
+                return true;
+
+            if (value > target)
+            {
+                return SearchMatrix(matrix, target, rowLow, rowMid - 1, columnLow, columnMid - 1) ||
+                    SearchMatrix(matrix, target, rowLow, rowMid - 1, columnMid, columnHigh) ||
+                    SearchMatrix(matrix, target, rowMid, rowHigh, columnLow, columnMid - 1);
+            }
+            else
+            {
+                return SearchMatrix(matrix, target, rowMid + 1, rowHigh, columnMid + 1, columnHigh) ||
+                    SearchMatrix(matrix, target, rowLow, rowMid, columnMid + 1, columnHigh) ||
+                    SearchMatrix(matrix, target, rowMid + 1, rowHigh, columnLow, columnMid);
+            }
         }
     }
 }
