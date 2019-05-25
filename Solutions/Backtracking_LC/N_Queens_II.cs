927195da1e79ebd2272bfc29144dd91247b7abe2
Destiny130 <destiny1307654641@hotmail.com>
2019-05-25 18:26:35 +0800

    Create N Queens II

---
+++ Solutions/Backtracking_LC/N_Queens_II.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Backtracking_LC
+{
+    public class N_Queens_II
+    {
+        public int TotalNQueens(int n)
+        {
+            int count = 0;
+
+            return count;
+        }
+    }
+}
