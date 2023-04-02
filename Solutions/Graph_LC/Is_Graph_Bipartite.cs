b7d87dfbedd1a848f0f8c41933513d1fa153ab67
Destiny130 <destiny1307654641@hotmail.com>
2023-04-02 10:56:26 +0800

    commit

---
+++ Solutions/Graph_LC/Is_Graph_Bipartite.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Graph_LC
+{
+    public class Is_Graph_Bipartite
+    {
+        public bool IsBipartite(int[][] graph)
+        {
+            int len = graph.Length;
+            int[] colors = new int[len];
+
+            for (int i = 0; i < len; ++i)
+            {
+                if (colors[i] != 0)
+                {
+                    continue;
+                }
+                colors[i] = 1;
+                Queue<int> queue = new Queue<int>();
+                queue.Enqueue(i);
+                while (queue.Count != 0)
+                {
+                    int curr = queue.Dequeue();
+                    int val = colors[curr];
+                    int[] arr = graph[curr];
+                    foreach (int j in arr)
+                    {
+                        if (colors[j] == 0)
+                        {
+                            colors[j] = -val;
+                            queue.Enqueue(j);
+                        }
+                        else if (colors[j] != -val)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- TestCases/Execute.cs
+++ TestCases/Execute.cs
@@ -9,10 +9,19 @@ namespace TestCases
     {
         static void Main()
         {
+            //double d0 = 100000, d1 = 100000, d2 = 100000;
+            //for (int i = 1; i <= 30; ++i)
+            //{
+            //    d0 *= 1.05;
+            //    d1 *= 1.15;
+            //    d2 = d2 * 1.15 + 50000;
+            //    Console.WriteLine($"{i}\t\t{d0:F3}\t\t{d1:F3}\t\t{d2:F3}");
+            //}
+
             #region Array
 
             //new Two_Sum_Test().Execute();
-            new _3Sum_Test().Execute();
+            //new _3Sum_Test().Execute();
 
             #endregion
 
@@ -28,7 +37,87 @@ namespace TestCases
 
             #endregion
 
+            //string name = "Rick";
+            //string intro1 = "My name is " + name;
+            //string intro2 = "My name is " + name;
+            //Console.WriteLine(intro1 == intro2);
+            //Console.WriteLine(AreReferenceEqual(intro1, intro2));
+
+            int loopCount = 1000000;
+
+            long num = 9223372036854775807;
+
+            var pingfang = Math.Pow(num, 2);
+            var lifagngen = Math.Sqrt(num);
+
+            DateTime startTime = DateTime.UtcNow;
+            int strLen = 0;
+            for (int i = 0; i < loopCount; ++i)
+            {
+                string str = num.ToString();
+                strLen = str.Length;
+            }
+            DateTime endTime = DateTime.UtcNow;
+            Console.WriteLine($"str.Length: {strLen}, spent: {(endTime - startTime).TotalMilliseconds} ms");
+
+            startTime = DateTime.UtcNow;
+            int len = 0;
+            for (int i = 0; i < loopCount; ++i)
+            {
+                int count = 0;
+                long curr = num;
+                while (curr > 0)
+                {
+                    count++;
+                    curr /= 10;
+                }
+                len = count;
+            }
+
+            endTime = DateTime.UtcNow;
+            Console.WriteLine($"num.Length: {len}, spent: {(endTime - startTime).TotalMilliseconds} ms");
+
+
             Console.ReadKey();
+        }
+
+        static bool AreReferenceEqual<T>(T first, T second) where T : class
+        {
+            return first == second;
+        }
+    }
+
+    public static class Program
+    {
+        public static void Test()
+        {
+            Base b = new Base();
+            b.Dispose();
+            ((IDisposable)b).Dispose();
+
+            Derived d = new Derived();
+            d.Dispose();
+            ((IDisposable)b).Dispose();
+
+            b = new Derived();
+            b.Dispose();
+            ((IDisposable)b).Dispose();
+        }
+    }
+
+    internal class Base : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Base's Dispose");
+        }
+    }
+
+    internal class Derived : Base, IDisposable
+    {
+        new public void Dispose()
+        {
+            Console.WriteLine("Derived's Dispose");
         }
     }
 }
